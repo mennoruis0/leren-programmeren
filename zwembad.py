3fab9661f2757479427e0969994ee2f52d6ac6f5
Menno Ruis <mennoruis@MacBook-Air-van-Menno.local>
2022-10-13 10:00:46 +0200

    stap 2

--- zwembad.py
+++ zwembad.py
@@ -4,9 +4,11 @@ diepte = float(input("hoe diep in meters: "))
 afmeting = lengte * breedte * diepte
 #print(afmeting)
 
+uitgraven = 1
+afvoeren = 1
 
 
 print(f' offerte voor zwembad van {lengte} bij {breedte} bij {diepte} meter (inhoud: xx m3) ')
-print('  uitgraven:        € xxx.xx')
-print('  afvoeren grond:   € xxx.xx')
-print('  totaal:           € xxx.xx')
+print(f' uitgraven:        € {uitgraven}')
+print(f' afvoeren grond:   € {afvoeren}')
+print(f' totaal:           € {uitgraven + afvoeren}')
