1e700ea7abf545ed87c1ca4ce11c18187d5272ff
Menno Ruis <mennoruis@MacBook-Air-van-Menno.local>
2022-10-13 09:51:28 +0200

    stap 2 zwembad

--- HetBos.py
+++ HetBos.py
@@ -1,8 +1,18 @@
-print("welkom bij de game: Het Bos")
+from asyncio import new_event_loop
+
+
+print("Welkom bij de game: Het Bos")
 print("Je zit in een vliegtuig naar het buitenland voor je werk.")
-print("je vliegtuig stort neer op een eilland je bent de enigste overlever in het vliegtuig.")
+print("Het vliegtuig stort neer op een eilland je bent de enigste overlever in het vliegtuig.")
 klimmen = input("je ziet een hoge boom wil je er in klimmen? ja/nee: ")
-    if klimmen == 'ja':
-        print("je ziet ergens rook tussen de bomen komen")
-    elif klimmen == "nee":
-elif 
+if klimmen == 'ja':
+        print("Het uitzicht is erg mooi je ziet een grote berg maar je ziet ook ergens rook tussen de bomen komen")
+elif klimmen == "nee":
+    print("Je ziet veel kleurrijke bessen in de struiken")
+bessen = input("Je hebt honger dus je hebt al wat besjes gevonden welke ga je eten: wolfskers/rozenbottel: ")
+if bessen == "wolfskers":
+    print("Helaas, het waren giftige bessen")
+    print("je bent nu dood")
+    print("EINDE")
+elif bessen == "rozenbottel":
+    print("De besjes waren erg lekker maar je voelt je nu wel moe")
--- zwembad.py
+++ zwembad.py
@@ -2,4 +2,11 @@ lengte = int(input("hoe lang in meters: "))
 breedte = int(input("hoe breed in meters: "))
 diepte = float(input("hoe diep in meters: "))
 afmeting = lengte * breedte * diepte
-print(afmeting)
+#print(afmeting)
+
+
+
+print(f' offerte voor zwembad van {lengte} bij {breedte} bij {diepte} meter (inhoud: xx m3) ')
+print('  uitgraven:        € xxx.xx')
+print('  afvoeren grond:   € xxx.xx')
+print('  totaal:           € xxx.xx')
