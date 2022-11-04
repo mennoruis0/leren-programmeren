c86e0a92b3de58941c113b7246bc0307f8a95875
Menno Ruis <mennoruis@MacBook-Air-van-Menno.local>
2022-11-04 10:02:28 +0100

    robotarm

--- robotarm-python-2021-main/opdracht 3.py
+++ robotarm-python-2021-main/opdracht 3.py
@@ -5,40 +5,20 @@ robotArm = RobotArm('exercise 3')
 robotArm.speed = 2
 
 robotArm.grab()
-for x in range(4):
-    robotArm.moveRight()
-robotArm.drop()
-for x in range(4):
-    robotArm.moveLeft()
-robotArm.grab()
-for x in range(3):
-    robotArm.moveRight()
-robotArm.drop()
-for x in range(3):
-    robotArm.moveLeft()
-robotArm.grab()
-for x in range(2):
-    robotArm.moveRight()
-robotArm.drop()
-for x in range(2):
-    robotArm.moveLeft()
-robotArm.grab()
 robotArm.moveRight()
 robotArm.drop()
-robotArm.moveRight()
-robotArm.grab()
 robotArm.moveLeft()
-robotArm.drop()
-for x in range(2):
-    robotArm.moveRight()
 robotArm.grab()
-for x in range(2):
-    robotArm.moveLeft()
+robotArm.moveRight()
 robotArm.drop()
-for x in range(3):
-    robotArm.moveRight()
+robotArm.moveLeft()
 robotArm.grab()
-for x in range(3):
-    robotArm.moveLeft()
+robotArm.moveRight()
 robotArm.drop()
+robotArm.moveLeft()
+robotArm.grab()
+robotArm.moveRight()
+robotArm.drop()
+
+
 robotArm.wait()
